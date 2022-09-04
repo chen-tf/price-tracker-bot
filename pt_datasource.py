2705d39539968e37b45fa39ba6cddaf73fa4a4cf
chen-tf <88727350+chen-tf@users.noreply.github.com>
2022-09-04 14:11:13 +0800

    Update pt_datasource.py

--- pt_datasource.py
+++ pt_datasource.py
@@ -6,7 +6,7 @@ from psycopg2.pool import ThreadedConnectionPool
 import pt_config
 
 psycopg2.extras.register_uuid()
-pool = ThreadedConnectionPool(2, 4, host=pt_config.DB_HOST, database=pt_config.DB_NAME, user=pt_config.DB_USER,
+pool = ThreadedConnectionPool(1, 3, host=pt_config.DB_HOST, database=pt_config.DB_NAME, user=pt_config.DB_USER,
                               password=pt_config.DB_PASSWORD)
 logging.info("Connection Pool established")
 
