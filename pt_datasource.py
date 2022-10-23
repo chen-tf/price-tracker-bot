f3770d93a5ecfb87034627018b5d3becd655611a
tim.tf <tim.tf.chen@gmail.com>
2022-10-23 20:05:06 +0800

    Update data source connection options

--- pt_datasource.py
+++ pt_datasource.py
@@ -6,8 +6,15 @@ from psycopg2.pool import ThreadedConnectionPool
 import pt_config
 
 psycopg2.extras.register_uuid()
-pool = ThreadedConnectionPool(1, 10, host=pt_config.DB_HOST, database=pt_config.DB_NAME, user=pt_config.DB_USER,
-                              password=pt_config.DB_PASSWORD)
+pool = ThreadedConnectionPool(1, 10, host=pt_config.DB_HOST,
+                              database=pt_config.DB_NAME,
+                              user=pt_config.DB_USER,
+                              password=pt_config.DB_PASSWORD,
+                              connect_timeout=3,
+                              keepalives=1,
+                              keepalives_idle=5,
+                              keepalives_interval=2,
+                              keepalives_count=2)
 logging.info("Connection Pool established")
 
 
