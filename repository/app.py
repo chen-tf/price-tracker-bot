3a317e68ef11144e6d8ab1398b4eb161c69572d3
Eddy <eddy15201@gmail.com>
2022-12-22 16:42:45 +0800

    Update database url

--- repository/app.py
+++ repository/app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 # 關閉追蹤各種改變的信號
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = \
+#     f"postgresql://{pt_config.DB_USER}:{pt_config.DB_PASSWORD}@{pt_config.DB_HOST}:5432/{pt_config.DB_NAME}"
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    f"postgresql://{pt_config.DB_USER}:{pt_config.DB_PASSWORD}@{pt_config.DB_HOST}:5432/eddy_db"
+    f"postgresql://{pt_config.DB_USER}:{pt_config.DB_PASSWORD}@{pt_config.DB_HOST}:5432/test"
 
 # 開啟可以看對應的sql語法
 # app.config['SQLALCHEMY_ECHO'] = True
