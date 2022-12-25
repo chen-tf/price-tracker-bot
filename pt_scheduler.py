194b3154b0753336a015b5392dfcb1f84cb269f2
Tim Chen <tim.tf.chen@linecorp.com>
2022-12-26 01:32:21 +0800

    merge scheduler app into web-api

--- pt_scheduler.py
+++ pt_scheduler.py
@@ -3,20 +3,16 @@ import threading
 import time
 
 import schedule
-from flask import Flask, Response
 
 import pt_config
 import pt_scheduler
 import pt_service
 
 logger = logging.getLogger('Scheduler')
-app = Flask(__name__)
 
-
-@app.route('/', methods=['GET'])
-def index():
-    # health check
-    return Response('OK', status=200)
+if __name__ == '__main__':
+    threading.Thread(target=pt_scheduler.my_job).start()
+    logger.info('Momo price tracker scheduler started.')
 
 
 def my_job():
@@ -25,7 +21,3 @@ def my_job():
     while True:
         schedule.run_pending()
         time.sleep(2)
-
-
-threading.Thread(target=pt_scheduler.my_job).start()
-logger.info('Momo price tracker scheduler started.')
