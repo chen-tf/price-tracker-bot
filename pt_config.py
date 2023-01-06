7c87f8fd026c6b8598c78f870bf34d24195b43f5
chen-tf <88727350+chen-tf@users.noreply.github.com>
2023-01-06 19:18:10 +0800

    extract db port (#61)

--- pt_config.py
+++ pt_config.py
@@ -4,37 +4,43 @@ import os
 from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-env_path = os.path.join(BASE_DIR, '.env')
+env_path = os.path.join(BASE_DIR, ".env")
 load_dotenv(dotenv_path=env_path, verbose=True)
 
-TELEGRAM_BOT_MODE = os.getenv('TELEGRAM_BOT_MODE', 'polling')
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-MOMO_URL = 'https://m.momoshop.com.tw/'
-MOMO_GOOD_URI = 'goods.momo'
-USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0'
+TELEGRAM_BOT_MODE = os.getenv("TELEGRAM_BOT_MODE", "polling")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MOMO_URL = "https://m.momoshop.com.tw/"
+MOMO_GOOD_URI = "goods.momo"
+USER_AGENT = (
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0"
+)
 
-WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 USER_SUB_GOOD_LIMITED = 11
 
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT", 5432)
 
-LOGGING_LEVEL = os.getenv('LOGGING_LEVEL')
-PERIOD_HOUR = int(os.getenv('PERIOD_HOUR', 2))
+LOGGING_LEVEL = os.getenv("LOGGING_LEVEL")
+PERIOD_HOUR = int(os.getenv("PERIOD_HOUR", 2))
 
 MOMO_REQUEST_TIMEOUT = 3
 
-LINE_NOTIFY_REDIRECT_URL = os.getenv('LINE_NOTIFY_REDIRECT_URL')
-LINE_NOTIFY_CLIENT_ID = os.getenv('LINE_NOTIFY_CLIENT_ID')
-LINE_NOTIFY_CLIENT_SECRET = os.getenv('LINE_NOTIFY_CLIENT_SECRET')
+LINE_NOTIFY_REDIRECT_URL = os.getenv("LINE_NOTIFY_REDIRECT_URL")
+LINE_NOTIFY_CLIENT_ID = os.getenv("LINE_NOTIFY_CLIENT_ID")
+LINE_NOTIFY_CLIENT_SECRET = os.getenv("LINE_NOTIFY_CLIENT_SECRET")
 
-PORT = int(os.getenv('PORT', 8080))
+PORT = int(os.getenv("PORT", 8080))
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=LOGGING_LEVEL, force=True)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=LOGGING_LEVEL,
+    force=True,
+)
 
 
 def momo_good_url():
--- pt_datasource.py
+++ pt_datasource.py
@@ -6,15 +6,20 @@ from psycopg2.pool import ThreadedConnectionPool
 import pt_config
 
 psycopg2.extras.register_uuid()
-pool = ThreadedConnectionPool(1, 8, host=pt_config.DB_HOST,
-                              database=pt_config.DB_NAME,
-                              user=pt_config.DB_USER,
-                              password=pt_config.DB_PASSWORD,
-                              connect_timeout=3,
-                              keepalives=1,
-                              keepalives_idle=5,
-                              keepalives_interval=2,
-                              keepalives_count=2)
+pool = ThreadedConnectionPool(
+    1,
+    8,
+    host=pt_config.DB_HOST,
+    port=pt_config.DB_PORT,
+    database=pt_config.DB_NAME,
+    user=pt_config.DB_USER,
+    password=pt_config.DB_PASSWORD,
+    connect_timeout=3,
+    keepalives=1,
+    keepalives_idle=5,
+    keepalives_interval=2,
+    keepalives_count=2,
+)
 logging.info("Connection Pool established")
 
 
