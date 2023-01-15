86646d4b389b9fc4c4b15e08e36ef0f9515885b3
chen-tf <88727350+chen-tf@users.noreply.github.com>
2023-01-15 12:31:58 +0800

    adjust scheduler method (#70)

--- pt_scheduler.py
+++ pt_scheduler.py
@@ -1,21 +1,15 @@
 import logging
-import threading
 import time
 
 import schedule
 
 import pt_config
-import pt_scheduler
 import pt_service
 
 logger = logging.getLogger("Scheduler")
 
 if __name__ == "__main__":
-    threading.Thread(target=pt_scheduler.my_job).start()
     logger.info("Momo price tracker scheduler started.")
-
-
-def my_job():
     schedule.every(pt_config.PERIOD_HOUR).hours.do(pt_service.sync_price)
     schedule.every(pt_config.PERIOD_HOUR).hours.do(pt_service.disable_not_active_user_sub_good)
     while True:
