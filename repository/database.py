21d602b7e8de85381e8900ae6ea5bf5f7d381eb2
timm <tim.tf.chen@gmail.com>
2023-02-21 22:35:18 +0800

    config SQLAlchemy connection pool

--- repository/database.py
+++ repository/database.py
@@ -6,7 +6,10 @@ import pt_config
 
 eng = create_engine(
     f"postgresql://{pt_config.DB_USER}:{pt_config.DB_PASSWORD}@"
-    f"{pt_config.DB_HOST}:{pt_config.DB_PORT}/{pt_config.DB_NAME}"
+    f"{pt_config.DB_HOST}:{pt_config.DB_PORT}/{pt_config.DB_NAME}",
+    pool_size=20,
+    max_overflow=0,
+    pool_pre_ping=True
 )
 autocommit_engine = eng.execution_options(isolation_level="AUTOCOMMIT")
 SessionLocal = sessionmaker(bind=autocommit_engine)
