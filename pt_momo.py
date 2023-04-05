b783873dbe2ffa6b512286a71eddd125e587f793
timm <tim.tf.chen@gmail.com>
2023-04-05 11:31:46 +0800

    add X-Forwarded-For header for TW IP

--- pt_momo.py
+++ pt_momo.py
@@ -90,7 +90,8 @@ def _get_good_info_from_momo(i_code=None):
                                "image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                      "accept-encoding": "gzip, deflate, br",
                      "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-                     "referer": "https://m.momoshop.com.tw/main.momo"},
+                     "referer": "https://m.momoshop.com.tw/main.momo",
+                     "X-Forwarded-For": "1.174.206.255"},
             timeout=pt_config.MOMO_REQUEST_TIMEOUT,
         )
         result = response.text
