24e69163b32a2a112b7d6bea2de572ae9e42caff
timm <tim.tf.chen@gmail.com>
2023-04-15 10:37:54 +0800

    update momo request header

--- pt_momo.py
+++ pt_momo.py
@@ -84,13 +84,20 @@ def _get_good_info_from_momo(i_code=None):
         response = session.get(
             pt_config.momo_good_url(),
             params=params,
-            headers={"user-agent": pt_config.USER_AGENT,
-                     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,"
-                               "image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                     "accept-encoding": "gzip, deflate, br",
-                     "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-                     "cache-control": "max-age=0",
-                     "referer": "https://m.momoshop.com.tw/category.momo?cn=4500000000&cid=dir&oid=dir&imgSH=fourCardStyle"},
+            headers={
+                'authority': 'm.momoshop.com.tw',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'zh-TW,zh;q=0.9',
+                'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"macOS"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'none',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+            },
             timeout=pt_config.MOMO_REQUEST_TIMEOUT,
         )
         result = response.text
