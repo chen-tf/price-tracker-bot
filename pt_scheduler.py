6bd617dfbfd51ad10eb29cd8cdaebe8a4a2bc42f
timm <tim.tf.chen@gmail.com>
2023-05-28 01:45:40 +0800

    use GitHub action schedule job

--- pt_scheduler.py
+++ pt_scheduler.py
@@ -1,9 +1,5 @@
 import logging
-import time
 
-import schedule
-
-import pt_config
 import pt_service
 
 logger = logging.getLogger("Scheduler")
@@ -11,8 +7,5 @@ logger = logging.getLogger("Scheduler")
 if __name__ == "__main__":
     logger.info("Momo price tracker scheduler started.")
     pt_service.sync_price()
-    schedule.every(pt_config.PERIOD_HOUR).hours.do(pt_service.sync_price)
-    schedule.every(pt_config.PERIOD_HOUR).hours.do(pt_service.disable_blocked_user_data)
-    while True:
-        schedule.run_pending()
-        time.sleep(2)
+    pt_service.disable_blocked_user_data()
+    logger.info("Momo price tracker scheduler finished.")
